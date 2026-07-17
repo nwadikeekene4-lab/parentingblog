aa960468ed25610c3529c0d5f9b3f4763623ed6b
nwadikeekene4-lab <nwadikeekene4@gmail.com>
2026-07-17 18:38:16 +0100

    Add SuccessStoriesPage with StoryCategoryLayout

--- src/app/stories/success-stories/page.tsx
+++ src/app/stories/success-stories/page.tsx
@@ -1,1 +1,11 @@
+import StoryCategoryLayout from "../components/StoryCategoryLayout";
 
+export default function SuccessStoriesPage() {
+  return (
+    <StoryCategoryLayout
+      title="Success Stories"
+      description="Celebrate inspiring parenting achievements and uplifting family stories from our community."
+      image="/Images/stories/parentingtips.jpeg"
+    />
+  );
+}
