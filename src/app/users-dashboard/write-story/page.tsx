fb36415bcbf5ede6156ca925ccc25a6e5bf0d097
nwadikeekene4-lab <nwadikeekene4@gmail.com>
2026-08-04 01:44:06 +0100

    Remove StoryTags and update comments in WriteStoryPage
    Removed StoryTags component and updated comments.

--- src/app/users-dashboard/write-story/page.tsx
+++ src/app/users-dashboard/write-story/page.tsx
@@ -4,14 +4,12 @@ import StoryEditor from "./components/StoryEditor";
 import CategorySelector from "./components/CategorySelector";
 import CoverImageUpload from "./components/CoverImageUpload";
 import StoryImages from "./components/StoryImages";
-import StoryTags from "./components/StoryTags";
 import StoryActions from "./components/StoryActions";
 import { StoryFormProvider } from "./components/StoryFormContext";
 
 export default function WriteStoryPage() {
   return (
     <StoryFormProvider>
-
       <div className="space-y-8">
 
         {/* Page Header */}
@@ -46,16 +44,11 @@ export default function WriteStoryPage() {
 
         <StoryImages />
 
-        {/* Story Tags */}
-
-        <StoryTags />
-
-        {/* Publish Buttons */}
+        {/* Publish Actions */}
 
         <StoryActions />
 
       </div>
-
     </StoryFormProvider>
   );
-            }
+      }
