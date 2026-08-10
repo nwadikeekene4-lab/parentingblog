b5b12cfb5da0a3ab9c53db66171a2f31f0713144
nwadikeekene4-lab <nwadikeekene4@gmail.com>
2026-08-10 21:00:29 +0100

    Update import path for BookmarkButton component

--- src/app/stories/components/StoryCategoryLayout.tsx
+++ src/app/stories/components/StoryCategoryLayout.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import BookmarkButton from "./BookmarkButton";
+import BookmarkButton from "../[slug]/BookmarkButton";
 
 type Story = {
   id: string;
