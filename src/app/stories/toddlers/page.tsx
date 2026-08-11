8306a32049ab392b20f6184cc8ed5d24eead4293
nwadikeekene4-lab <nwadikeekene4@gmail.com>
2026-08-11 01:11:19 +0100

    Refactor ToddlersPage for async story fetching
    Updated the ToddlersPage to fetch stories asynchronously and modified the description and image.

--- src/app/stories/toddlers/page.tsx
+++ src/app/stories/toddlers/page.tsx
@@ -1,11 +1,19 @@
 import StoryCategoryLayout from "../components/StoryCategoryLayout";
+import { getStoriesByCategory } from "@/lib/getStoriesByCategory";
 
-export default function ToddlersPage() {
+export default async function ToddlersPage() {
+
+  const stories =
+    await getStoriesByCategory(
+      "Toddlers"
+    );
+
   return (
     <StoryCategoryLayout
       title="Toddlers"
-      description="Real parenting stories about raising toddlers, learning milestones and everyday adventures."
-      image="/Images/stories/toddlerorg.jpeg"
+      description="Discover real stories about raising toddlers, navigating challenges and celebrating their amazing milestones."
+      image="/Images/stories/toddler.jpg"
+      stories={stories}
     />
   );
 }
