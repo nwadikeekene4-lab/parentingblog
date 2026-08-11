368f16d1af031719a5783c57598a1fd467fb9d3a
nwadikeekene4-lab <nwadikeekene4@gmail.com>
2026-08-11 01:10:13 +0100

    Refactor NewbornPage to fetch stories asynchronously
    Updated NewbornPage to fetch stories asynchronously and modified description and image.

--- src/app/stories/newborn/page.tsx
+++ src/app/stories/newborn/page.tsx
@@ -1,11 +1,19 @@
 import StoryCategoryLayout from "../components/StoryCategoryLayout";
+import { getStoriesByCategory } from "@/lib/getStoriesByCategory";
 
-export default function NewbornPage() {
+export default async function NewbornPage() {
+
+  const stories =
+    await getStoriesByCategory(
+      "Newborn"
+    );
+
   return (
     <StoryCategoryLayout
       title="Newborn"
-      description="Discover heartfelt stories, sleepless nights, joyful milestones and experiences from families welcoming a newborn."
-      image="/Images/stories/newborn.jpeg"
+      description="Read real experiences about the joys, challenges and unforgettable moments of caring for a newborn."
+      image="/Images/stories/newborn.jpg"
+      stories={stories}
     />
   );
 }
