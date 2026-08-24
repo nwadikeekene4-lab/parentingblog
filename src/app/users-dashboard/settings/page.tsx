5f682c478d38a5d912dbe822e68298660bbf98d1
nwadikeekene4-lab <nwadikeekene4@gmail.com>
2026-08-24 13:40:52 +0100

    Update page.tsx

--- src/app/users-dashboard/settings/page.tsx
+++ src/app/users-dashboard/settings/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function SettingsPage() {
   const [emailNotifications, setEmailNotifications] =
